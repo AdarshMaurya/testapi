53bab10db629c2e90a3a56869bc067afc4b509dc
Adarsh Maurya <adarshmauryatalks@gmail.com>
2018-08-24 00:23:15 +0530

    -added few more test cases

--- src/test/java/com/softhinkers/script/sanitysuite/SampleTest.java
+++ src/test/java/com/softhinkers/script/sanitysuite/SampleTest.java
@@ -1,47 +1,68 @@
 package com.softhinkers.script.sanitysuite;
 
-import com.softhinkers.script.base.TestBase;
+import com.softhinkers.script.base.BaseTest;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
-import static com.softhinkers.script.util.Constants.TEST_FOO;
-import static com.softhinkers.script.util.XmlParser.checkXml;
+import static com.softhinkers.script.utils.Constants.TEST_FOO;
+import static com.softhinkers.script.utils.XmlParser.checkXml;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.fail;
 
-public class SampleTest extends TestBase {
+public class SampleTest extends BaseTest {
 
     Request request = null;
     Response response = null;
 
-    @AfterMethod
-    public void setUp(){
-
-    }
-
-    @AfterMethod
-    public void responseClose() {
-        response.body().close();
-    }
-
-    @Test(priority = 1, description = "Fetch Xml File")
-    public void fetchXmlFile() throws IOException {
-        log.info("Fetch Xml File.");
-        //for(int i =0;i<=10; i++) {
+    @BeforeMethod
+    public void setUp() throws IOException {
+        log.info("Before Method Set Up.");
         request = new Request.Builder().url(TEST_FOO).build();
         response = client.newCall(request).execute();
         if (!response.isSuccessful()) {
             fail("Response code " + response.code());
             return;
         }
+
+    }
+
+    @AfterMethod
+    public void responseClose() {
+        log.info("After Method Set Up.");
+        response.body().close();
+    }
+
+    @Test(priority = 1, description = "Test to Fetch First Name")
+    public void testToFetchFirstName() throws IOException {
+        log.info("Test to Fetch First Name");
         log.info("Response code " + response.code());
         String xml = response.body().string();
-        assertEquals(checkXml(xml,"firstname", "sample_name"),true);
+        checkXml(xml,"firstname", "sample_name");
 
         // }
     }
+
+    @Test(priority = 1, description = "Test To Fetch Last Name")
+    public void testToFetchLastName() throws IOException {
+        log.info("Test To Fetch Last Name");
+        log.info("Response code " + response.code());
+        String xml = response.body().string();
+        checkXml(xml,"lastname", "sample_names");
+    }
+
+    @Test(priority = 1, description = "Test To Fetch State Name")
+    public void testToFetchStateName() throws IOException {
+        log.info("Test To Fetch Last Name");
+        log.info("Response code " + response.code());
+        String xml = response.body().string();
+        checkXml(xml,"state", "sample_state_name");
+    }
+
+
 }
