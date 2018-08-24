79e6d4c5111814a59e5bd6326ecd2bc360d0363b
Adarsh Maurya <adarshmauryatalks@gmail.com>
2018-08-25 01:55:22 +0530

    - added repeatTestGetXMLResponseBody & sendRequestMethod. Together they are used  when test name which contains 'fileTest' invokes

--- src/test/java/com/softhinkers/script/sanitysuite/SampleTest2.java
+++ src/test/java/com/softhinkers/script/sanitysuite/SampleTest2.java
@@ -10,65 +10,70 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.net.URLEncoder;
+import java.util.List;
 
 import static com.softhinkers.script.utils.Constants.BASE_URL;
 import static com.softhinkers.script.utils.XmlParser.checkXml;
 import static org.testng.Assert.fail;
 
 public class SampleTest2 extends BaseTest {
-
     Request request = null;
     Response response = null;
-    String testUrl = null;
 
     @BeforeMethod
-    public void setUp(ITestContext context) {
+    public void setUp() {
         log.info("Before Method Set Up.");
     }
 
-    @Test(description = "Test to Fetch Last Name", invocationCount = 10)//, invocationCount=10, threadPoolSize=3
-    public void testToFetchLastName(ITestContext context) throws IOException {
-        invokeTestNumber(context, 0);
-
-        request = new Request.Builder().url(testUrl).build();
-        response = client.newCall(request).execute();
-        if (!response.isSuccessful()) {
-            fail("Response code " + response.code());
-            return;
-        }
-        String xml = response.body().string();
+    @Test(description = "Test to Fetch Last Name")// threadPoolSize=3
+    public void fileTestToFetchLastName(ITestContext context, Method method) throws IOException {
+        String xml = repeatTestGetXMLResponseBody(context, method, urlList);
         checkXml(xml, "lastname", "sample_name");
-
     }
 
-    @Test(description = "Test to Fetch State Name", invocationCount = 10)//, invocationCount=10, threadPoolSize=3
-    public void testToFetchStateName(ITestContext context) throws IOException {
-        invokeTestNumber(context, 1);
-
-        request = new Request.Builder().url(testUrl).build();
-        response = client.newCall(request).execute();
-        if (!response.isSuccessful()) {
-            fail("Response code " + response.code());
-            return;
-        }
-        String xml = response.body().string();
+    @Test(description = "Test to Fetch State Name")//threadPoolSize=3
+    public void fileTestToFetchStateName(ITestContext context, Method method) throws IOException {
+        String xml = repeatTestGetXMLResponseBody(context, method, urlList);
         checkXml(xml, "state", "sample_state_name");
     }
 
     @AfterMethod
     public void tearDown() {
         log.info("After Method Set Up.");
-        response.body().close();
     }
 
 
-    private void invokeTestNumber(ITestContext context, int testNumber) {
-        int currentCount = context.getAllTestMethods()[testNumber].getCurrentInvocationCount();
-        if (urlList != null) {
-            testUrl = BASE_URL + urlList.get(currentCount);
-        } else {
-            testUrl = BASE_URL;
+    private String repeatTestGetXMLResponseBody(ITestContext context, Method method, List<String> urlList) throws IOException {
+        String testUrl = null;
+        if (urlList != null && urlList.size() > 1) {
+            ITestNGMethod currentTestNGMethod = null;
+            for (ITestNGMethod testNGMethod : context.getAllTestMethods()) {
+                if (testNGMethod.getMethodName().equals(method.getName())) {
+                    currentTestNGMethod = testNGMethod;
+                    break;
+                }
+            }
+            assert currentTestNGMethod != null;
+            testUrl = BASE_URL + urlList.get(currentTestNGMethod.getCurrentInvocationCount());
         }
+        return sendRequest(testUrl);
     }
 
+    private String sendRequest(String testUrl) throws IOException {
+        if (testUrl != null) {
+            request = new Request.Builder().url(testUrl).build();
+            response = client.newCall(request).execute();
+            if (!response.isSuccessful()) {
+                fail("Response code " + response.code());
+                return null;
+            }
+            String responseBody = response.body().string();
+            response.body().close();
+            return responseBody;
+
+        }
+        return null;
+    }
 }
